d65ee56a9a78fd042a7d6a696e6ca724faee8d99
Anh Phu <anhphu195@gmail.com>
2019-02-11 13:50:40 +0100

    Start Project

---
+++ happybay/src/main/java/com/propra/happybay/HappybayApplication.java
@@ -0,0 +1,14 @@
+package com.propra.happybay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappybayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappybayApplication.class, args);
+    }
+
+}
+
