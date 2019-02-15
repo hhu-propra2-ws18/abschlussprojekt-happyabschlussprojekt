3c2a35dc6c3a01192c0fdba4e90003f2a2348c0a
Kerui Zhang <kerui.zhang@uni-duesseldorf.de>
2019-02-15 14:58:23 +0100

    add person Foto

--- happybay/src/main/java/com/propra/happybay/Controller/HappyBayController.java
+++ happybay/src/main/java/com/propra/happybay/Controller/HappyBayController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.sound.midi.SysexMessage;
+import java.io.File;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
@@ -50,8 +51,9 @@ public class HappyBayController {
     }
 
     @PostMapping("/add")
-    public String addToDatabase(@ModelAttribute("person") Person person, BindingResult bindingResult,
-                                Model model) {
+    public String addToDatabase(@RequestParam("file") MultipartFile file,
+                                @ModelAttribute("person") Person person, BindingResult bindingResult,
+                                Model model) throws IOException{
         userValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors()) {
             List<String> errorList = new ArrayList<>();
@@ -62,6 +64,9 @@ public class HappyBayController {
             model.addAttribute("errorList", errorList);
             return "addUser";
         }
+        Bild bild = new Bild();
+        bild.setBild(file.getBytes());
+        person.setFoto(bild);
         person.setRole("ROLE_USER");
         person.setPassword(encoder.encode(person.getPassword()));
         personRepository.save(person);
@@ -94,6 +99,7 @@ public class HappyBayController {
     public String profile(Model model, Principal principal) {
         String name = principal.getName();
         Person person = personRepository.findByUsername(name).get();
+        person.setEncode(encodeBild(person.getFoto()));
         model.addAttribute("user", person);
         return "profile";
     }
@@ -146,12 +152,6 @@ public class HappyBayController {
     public String confirmGeraet(@ModelAttribute("geraet") Geraet geraet,
                                 @RequestParam("files") MultipartFile[] files, Principal person) throws IOException {
         List<Bild> bilds = new ArrayList<>();
-        for(int i = 0;i<files.length;i++){
-            Bild bild = new Bild();
-            bild.setBild(files[i].getBytes());
-
-        }
-
         for (MultipartFile file : files) {
             Bild bild = new Bild();
             bild.setBild(file.getBytes());
@@ -210,7 +210,7 @@ public class HappyBayController {
     }
 
     @GetMapping("/PersonInfo/Profile/ChangeProfile")
-    public String changeImg(Model model, Principal principal) {
+    public String changeImg(Model model, Principal principal){
         String name = principal.getName();
         Person person = personRepository.findByUsername(name).get();
         model.addAttribute("person", person);
@@ -218,9 +218,13 @@ public class HappyBayController {
     }
 
     @PostMapping("/PersonInfo/Profile/ChangeProfile")
-    public String chageProfile(@ModelAttribute("person") Person p, Principal principal) {
+    public String chageProfile(@RequestParam("file") MultipartFile file,
+                               @ModelAttribute("person") Person p, Principal principal) throws IOException {
         String name = principal.getName();
         Person person = personRepository.findByUsername(name).get();
+        Bild bild = new Bild();
+        bild.setBild(file.getBytes());
+        person.setFoto(bild);
         person.setNachname(p.getNachname());
         person.setKontakt(p.getKontakt());
         person.setVorname(p.getVorname());
