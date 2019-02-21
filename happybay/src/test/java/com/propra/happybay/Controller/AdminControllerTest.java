9be32c7a0c2654e40a2708380fd550b9d10e0a6d
nguyenthanhtungn5 <nguyenthanhtungn5@gmail.com>
2019-02-21 13:21:47 +0100

    Admin Controller Test

---
+++ happybay/src/test/java/com/propra/happybay/Controller/AdminControllerTest.java
@@ -0,0 +1,44 @@
+package com.propra.happybay.Controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdminControllerTest {
+
+    @Test
+    public void adminFunktion() {
+    }
+
+    @Test
+    public void allUsers() {
+    }
+
+    @Test
+    public void conflicts() {
+    }
+
+    @Test
+    public void adminNotifications() {
+    }
+
+    @Test
+    public void erhoeheAmount() {
+    }
+
+    @Test
+    public void punishAccount() {
+    }
+
+    @Test
+    public void releaseAccount() {
+    }
+
+    @Test
+    public void aufladenAntrag() {
+    }
+
+    @Test
+    public void changePassword() {
+    }
+}
