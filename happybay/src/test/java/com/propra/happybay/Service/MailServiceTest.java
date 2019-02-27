797a0e3aa14a74982c7b7fad04ccd8cda7004f8e
Kerui Zhang <kerui.zhang@uni-duesseldorf.de>
2019-02-27 13:42:17 +0100

    mailservice test

--- happybay/src/test/java/com/propra/happybay/Service/MailServiceTest.java
+++ happybay/src/test/java/com/propra/happybay/Service/MailServiceTest.java
@@ -2,9 +2,11 @@ package com.propra.happybay.Service;
 
 import com.propra.happybay.Model.Geraet;
 import com.propra.happybay.Model.Person;
+import com.propra.happybay.Model.RentEvent;
 import com.propra.happybay.Repository.GeraetRepository;
 import com.propra.happybay.Repository.PersonRepository;
 import com.propra.happybay.Repository.RentEventRepository;
+import com.propra.happybay.ReturnStatus;
 import com.propra.happybay.Service.UserServices.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,8 +20,11 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import javax.mail.internet.MimeMessage;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -41,34 +46,37 @@ public class MailServiceTest {
     MailService mailService;
 
 
-//    @Test
-//    public void send_Scheduled_Mail() throws Exception {
-//        List<RentEvent> rentEventList = new ArrayList<>();
-//        RentEvent rentEvent1 = new RentEvent();
-//        rentEvent1.setGeraetId(1L);
-//        rentEvent1.setReturnStatus(ReturnStatus.DEADLINE_CLOSE);
-//        rentEventList.add(rentEvent1);
-//
-//        RentEvent rentEvent2 = new RentEvent();
-//        rentEvent2.setGeraetId(2L);
-//        rentEvent2.setReturnStatus(ReturnStatus.DEADLINE_OVER);
-//        rentEventList.add(rentEvent2);
-//        Mockito.when(rentEventRepository.findAll()).thenReturn(rentEventList);
-//
-//        Person fakePerson = new Person();
-//        fakePerson.setKontakt("a@b.c");
-//        Mockito.when(personRepository.findByUsername(any())).thenReturn(java.util.Optional.of(fakePerson));
-//        MimeMessage msg = new JavaMailSenderImpl().createMimeMessage();
-//        Mockito.when(sender.createMimeMessage()).thenReturn(msg);
-//
-//        Geraet fakeGeraet = new Geraet();
-//        fakeGeraet.setTitel("fake Geraet");
-//        Mockito.when(geraetRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(fakeGeraet));
-//
-//        mailService.sendScheduledMail();
-//        verify(sender,times(2)).createMimeMessage();
-//        verify(sender,times(2)).send((MimeMessage) any());
-//    }
+    @Test
+    public void send_Scheduled_Mail() throws Exception {
+        List<RentEvent> rentEventList = new ArrayList<>();
+        RentEvent rentEvent1 = new RentEvent();
+        rentEvent1.setGeraetId(1L);
+        rentEvent1.setMieter("fake Mieter");
+        rentEvent1.setReturnStatus(ReturnStatus.DEADLINE_CLOSE);
+        rentEventList.add(rentEvent1);
+
+        RentEvent rentEvent2 = new RentEvent();
+        rentEvent2.setGeraetId(2L);
+        rentEvent2.setMieter("fake Mieter");
+        rentEvent2.setReturnStatus(ReturnStatus.DEADLINE_OVER);
+        rentEventList.add(rentEvent2);
+        Mockito.when(rentEventRepository.findAll()).thenReturn(rentEventList);
+
+        Person fakePerson = new Person();
+        fakePerson.setKontakt("a@b.c");
+        Mockito.when(personRepository.findByUsername(any())).thenReturn(java.util.Optional.of(fakePerson));
+        MimeMessage msg = new JavaMailSenderImpl().createMimeMessage();
+        Mockito.when(sender.createMimeMessage()).thenReturn(msg);
+
+        Geraet fakeGeraet = new Geraet();
+        fakeGeraet.setTitel("fake Geraet");
+        Mockito.when(geraetRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(fakeGeraet));
+
+        mailService.sendScheduledMail();
+        verify(sender,times(2)).createMimeMessage();
+        verify(sender,times(2)).send((MimeMessage) any());
+    }
+
 
     @Test
     public void send_mail() throws Exception {
